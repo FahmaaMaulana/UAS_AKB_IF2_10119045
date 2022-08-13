58bde825b43088e701cb60c9f8a5f03ff25184dd
FahmaaMaulana <fahmamaulana20@gmail.com>
2022-08-13 22:38:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uas_akb_if2_10119045/ui/AboutActivity.java
@@ -0,0 +1,20 @@
+package com.example.uas_akb_if2_10119045.ui;
+/**
+ * Nama : Fahma Maulana
+ * Kelas : IF2
+ * NIM :10119045
+ * Email : fahmamaulana20@gmail.com
+ * **/
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.uas_akb_if2_10119045.R;
+import androidx.fragment.app.Fragment;
+public class AboutActivity extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_about, container, false);
+    }}
